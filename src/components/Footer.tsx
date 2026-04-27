e1c716999296ece41333e8bf32dbf18831fb49b2
BasByte <netdroid007@gmail.com>
2026-04-27 22:38:42 +0100

    fix icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import { Code2, Github, Twitter, Linkedin, Mail } from 'lucide-react';
+
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
